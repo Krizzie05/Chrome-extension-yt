e52f76d0c2f5dcfb5e0b9b5e423c8270753621db
Krishnendu <krishnendunayek15@gmail.com>
2025-08-09 04:58:36 +0530

    Backend

--- app/core/config.py
+++ app/core/config.py
@@ -1,15 +1,15 @@
-import os
-from dotenv import load_dotenv
+# import os
+# from dotenv import load_dotenv
 
-# Load variables from .env file
-load_dotenv()
+# # Load variables from .env file
+# load_dotenv()
 
-class Settings:
-    # OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-    BACKEND_HOST = os.getenv("BACKEND_HOST", "0.0.0.0")
-    BACKEND_PORT = int(os.getenv("BACKEND_PORT", 8000))
-    CORS_ORIGINS = os.getenv("CORS_ORIGINS", "*").split(",")  # e.g., "http://localhost:3000,http://localhost:8080"
-    CHROMA_DB_PATH = os.getenv("CHROMA_DB_PATH", "./chroma_db")
-    YOUTUBE_TRANSCRIPTS_PATH = os.getenv("YOUTUBE_TRANSCRIPTS_PATH", "./transcripts")
+# class Settings:
+#     # OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+#     BACKEND_HOST = os.getenv("BACKEND_HOST", "0.0.0.0")
+#     BACKEND_PORT = int(os.getenv("BACKEND_PORT", 8000))
+#     CORS_ORIGINS = os.getenv("CORS_ORIGINS", "*").split(",")  # e.g., "http://localhost:3000,http://localhost:8080"
+#     CHROMA_DB_PATH = os.getenv("CHROMA_DB_PATH", "./chroma_db")
+#     YOUTUBE_TRANSCRIPTS_PATH = os.getenv("YOUTUBE_TRANSCRIPTS_PATH", "./transcripts")
 
-settings = Settings()
+# settings = Settings()
